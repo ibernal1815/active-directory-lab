3a978debe77ea92a11306762775c675dc0dae453
Isaiah Bernal <isaiahbernal750@outlook.com>
2025-09-26 10:40:40 -0700

    Update bulk_user_creation.ps1

--- scripts/bulk_user_creation.ps1
+++ scripts/bulk_user_creation.ps1
@@ -1,1 +1,26 @@
+# Bulk create AD users from a simple CSV
+# CSV format: FirstName,LastName,Username,Department
 
+param([string]$CsvPath = ".\users.csv")
+
+Import-Module ActiveDirectory
+$DefaultPassword = ConvertTo-SecureString '$silverline2025$' -AsPlainText -Force
+
+$users = Import-Csv $CsvPath
+
+foreach ($u in $users) {
+    $ou = "OU=$($u.Department),OU=Silverline Technologies,DC=silverline,DC=local"
+    $upn = "$($u.Username)@silverline.local"
+
+    New-ADUser -Name "$($u.FirstName) $($u.LastName)" `
+        -SamAccountName $u.Username `
+        -UserPrincipalName $upn `
+        -GivenName $u.FirstName `
+        -Surname $u.LastName `
+        -Path $ou `
+        -AccountPassword $DefaultPassword `
+        -Enabled $true
+
+    Add-ADGroupMember -Identity "$($u.Department)_Group" -Members $u.Username
+    Write-Host "Created user: $($u.Username) in $ou"
+}
